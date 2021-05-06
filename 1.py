7f96d67c52b370e053da180e66a60ee36762ce1a
ktakuya15 <soccer.1998.0616.15@gmail.com>
2021-05-06 13:25:07 +0900

    first commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです。
+"""
+
+import matplotlib.pyplot as plt
+
+x_list = [ 5,  6,  7,  8,  9]
+y_list = [50, 40, 30, 20, 10]
+
+plt.plot(x_list, y_list)
+plt.show()
